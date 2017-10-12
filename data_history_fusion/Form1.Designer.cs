b7f0bc5db6909cfd74d0612a364744fa6d41eb76
DVX opensource projects <reunisoft@gmail.com>
2017-10-12 23:34:23 +0200

    Add files via upload

---
+++ data_history_fusion/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace IKHRCDataFusion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Fusion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(93, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(834, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 281);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "IKH Research Center Data Fusion v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ data_history_fusion/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IKHRCDataFusion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C: \Users\InvesdataSystems\Documents\NetBeansProjects\investdata\public_html\alerts\data_history\";
+            IEnumerable<string> files = Directory.EnumerateFiles(path);
+            List<string> onedataline = new List<string>();
+
+            foreach (string str in files)
+            {
+                //log(str);
+
+                FileStream fs = new FileStream(str, FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string oneline = sr.ReadLine();
+                //1ere ligne = "Timestamp;Name;Buy;Sell;Spread";
+                //détéction du séparateur ; ou ,
+                string sep = "";
+                bool fileOkForReading = false;
+                if (oneline.Contains(";"))
+                {
+                    sep = ";";
+                    fileOkForReading = true;
+                }
+                else if (oneline.Contains(","))
+                {
+                    sep = ",";
+                    fileOkForReading = true;
+                }
+                else
+                {
+                    //MessageBox.Show("Skipping file " + str + " : Separator could not be parsed from line one");
+                    fileOkForReading = false;
+                }
+
+                //log("Separator detected = '" + sep + "'");
+
+                if (fileOkForReading)
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string s = sr.ReadLine();
+                        onedataline.Add(s);
+                    }
+                }
+                sr.Close();
+                fs.Close();
+            }
+
+            log(onedataline.Count() + " lines of data read");
+
+            DateTime timestamp = DateTime.Now;
+            string ts = timestamp.Year.ToString() + timestamp.Month.ToString("00") + timestamp.Day.ToString("00") + timestamp.Hour.ToString("00") + timestamp.Minute.ToString("00") + timestamp.Second.ToString("00");
+
+            string outputSeparator = ",";
+
+            if (File.Exists(path + @"\" + "ikh-output.csv"))
+            {
+                File.Delete(path + @"\" + "ikh-output.csv");
+            }
+            FileStream fso = new FileStream(path + @"\" + ts + "-ikh-output.csv", FileMode.CreateNew);
+            StreamWriter sw = new StreamWriter(fso);
+            if (outputSeparator == ",")
+            {
+                sw.WriteLine("Timestamp,Name,Buy,Sell,Spread");
+            } else if (outputSeparator == ";")
+            {
+                sw.WriteLine("Timestamp;Name;Buy;Sell;Spread");
+            }
+            for (int i = 0; i < onedataline.Count(); i++)
+            {
+                if ((i>0)&&((i%250000==0)||(i>onedataline.Count()-1)))
+                {
+                    sw.Close();
+                    fso.Close();
+                    fso = new FileStream(path + @"\" + ts + "-ikh-output" + i + ".csv", FileMode.CreateNew);
+                    sw = new StreamWriter(fso);
+                    if (outputSeparator == ",")
+                    {
+                        sw.WriteLine("Timestamp,Name,Buy,Sell,Spread");
+                    }
+                    else if (outputSeparator == ";")
+                    {
+                        sw.WriteLine("Timestamp;Name;Buy;Sell;Spread");
+                    }
+                }
+                string stroutput = "";
+                if (outputSeparator == ";")
+                {
+                    stroutput = onedataline[i].Replace(",", ";");
+                }
+                else if (outputSeparator == ",")
+                {
+                    stroutput = onedataline[i].Replace(";", ",");
+                }
+                sw.WriteLine(stroutput);
+            }
+            sw.Close();
+            fso.Close();
+        }
+
+        private void log(string str)
+        {
+            listBox1.Items.Add(str);
+        }
+    }
+}
